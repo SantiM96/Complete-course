267e2e7b866b163f7c800e1f310562f8b3796eec
SantiM96 <smt093@gmail.com>
2020-08-05 20:56:40 -0300

    ccs to register and initial js for him

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
-let number1 = 10,
-    number2 = "20",
-    number3 = "tres";
+(function () {
+    'use strict';
+    document.addEventListener('DOMContentLoaded', function () {
 
-let result;
+        // user dates
+        var nameUser = document.getElementById('name');
+        var surnameUser = document.getElementById('surname');
+        var emailUser = document.getElementById('email');
 
-result = number1 + Number(number3);
+        // order pass
+        var dayPass = document.getElementById('day-pass');
+        var allDayPass = document.getElementById('all-day-pass');
+        var twoDayPass = document.getElementById('two-days-pass');
+
+        // buttons and divs
+        var calcular = document.getElementById('caluclar');
+        var errorDiv = document.getElementById('error');
+        var buttonRegister = document.getElementById('btnregistro');
+        var result = document.getElementById('lista-productos');
 
 
-console.log(result);
-``
+
+
+
+    });
+})();
