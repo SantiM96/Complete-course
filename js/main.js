95f59200fde5907903e9faed763352704269bf32
SantiM96 <smt093@gmail.com>
2020-09-15 18:38:57 -0300

    fix bug with the another pages

--- js/main.js
+++ js/main.js
@@ -76,228 +76,212 @@ $(function() {
 
 
     //Countdown
-    $('.countdown').countdown('2020/9/11 12:00:00', function(event) { 
+    $('.countdown').countdown('2021/9/11 12:00:00', function(event) { 
         $('#days').html(event.strftime('%D'));
         $('#hours').html(event.strftime('%H'));
         $('#minutes').html(event.strftime('%M'));
         $('#seconds').html(event.strftime('%S'));
     })
 
-
-
-
-
-
-
-
-
 });
 
-
-
-
-
-
-
-
-/*
 
 (function() {
     'use strict';
    
-    document.addEventListener('DOMContentLoaded', function () {     
-        
-        // user dates -34.905572, -56.185498 GdlWebCamp
-        const nameUser = document.getElementById('name'),
-              surnameUser = document.getElementById('surname'),
-              emailUser = document.getElementById('email');
-        
-        //Check content dates camps
-        let camp1Complete = true,
-            camp2Complete = true,
-            camp3Complete = true;
+    document.addEventListener('DOMContentLoaded', function () {
+        if (document.getElementById('all-day-pass')) {
+                
+            // user dates -34.905572, -56.185498 GdlWebCamp
+            const nameUser = document.getElementById('name'),
+                surnameUser = document.getElementById('surname'),
+                emailUser = document.getElementById('email');
+            
+            //Check content dates camps
+            let camp1Complete = true,
+                camp2Complete = true,
+                camp3Complete = true;
 
-        // order pass
-        let dayPass = document.getElementById('day-pass'),
-            allDayPass = document.getElementById('all-day-pass'),
-            twoDayPass = document.getElementById('two-days-pass');
-        
-        let shirt = document.getElementById('camisa-evento'),
-            labels = document.getElementById('etiquetas');
-        
-        let numberDayPass = dayPass.value,
-            numberAllDayPass = allDayPass.value,
-            numberTwoDayPass = twoDayPass.value,
-            numberShirt = shirt.value,
-            numberLabels = labels.value;
-        
-        let totalAmountTickets = 0,
-            productList = [],
-            showDays = [];
-
-        // buttons and divs
-        let calculate = document.getElementById('calcular'),
-            gift = document.getElementById('regalo'),
-            errorP = document.getElementById('error'),
-            errorPMail = document.getElementById('errorMail'),
-            buttonRegister = document.getElementById('btnregistro'),
-            total = document.getElementById('suma-total'),
-            result = document.getElementById('lista-productos');
-        
-        
-        calculate.addEventListener('click', calculateAmount);
-
-        dayPass.addEventListener('blur', showCheckbox);
-        allDayPass.addEventListener('blur', showCheckbox);
-        twoDayPass.addEventListener('blur', showCheckbox);
-
-
-        nameUser.addEventListener('blur', emptyCamp);
-        surnameUser.addEventListener('blur', emptyCamp);
-        emailUser.addEventListener('blur', emptyCamp);
-        emailUser.addEventListener('blur', checkMail);
-
-
-
-        function calculateAmount(event) {
-            event.preventDefault();
-            if (gift.value === '') {
-                alert('Debes elegir un regalo');
-                gift.focus();
-            }
-            else {
-                dayPass = document.getElementById('day-pass');
-                allDayPass = document.getElementById('all-day-pass');
+            // order pass
+            let dayPass = document.getElementById('day-pass'),
+                allDayPass = document.getElementById('all-day-pass'),
                 twoDayPass = document.getElementById('two-days-pass');
-
-                shirt = document.getElementById('camisa-evento');
+                
+            let shirt = document.getElementById('camisa-evento'),
                 labels = document.getElementById('etiquetas');
-
-                numberDayPass = dayPass.value;
-                numberAllDayPass = allDayPass.value;
-                numberTwoDayPass = twoDayPass.value;
-                numberShirt = shirt.value;
+                
+            let numberDayPass = dayPass.value,
+                numberAllDayPass = allDayPass.value,
+                numberTwoDayPass = twoDayPass.value,
+                numberShirt = shirt.value,
                 numberLabels = labels.value;
-
                 
+            let totalAmountTickets = 0,
+                productList = [],
+                showDays = [];
 
-                totalAmountTickets = 0;
-
-                totalAmountTickets = numberDayPass * 30 + numberAllDayPass * 50
-                    + numberTwoDayPass * 45 + numberShirt * 10 * 0.93
-                    + numberLabels * 2;
+            // buttons and divs
+            let calculate = document.getElementById('calcular'),
+                gift = document.getElementById('regalo'),
+                errorP = document.getElementById('error'),
+                errorPMail = document.getElementById('errorMail'),
+                buttonRegister = document.getElementById('btnregistro'),
+                total = document.getElementById('suma-total'),
+                result = document.getElementById('lista-productos');
                 
-                productList = [];
-                // Add products to Array
-                if (numberDayPass == 1) {
-                    productList.push(numberDayPass + ' Pase por Día');
-                }
-                else if (numberDayPass > 1) {
-                    productList.push(numberDayPass + ' Pases por Día');
-                }
-
-                if (numberAllDayPass == 1) {
-                    productList.push(numberAllDayPass + ' Pase Completo');
-                }
-                else if (numberAllDayPass > 1) {
-                    productList.push(numberAllDayPass + ' Pases Completos');
-                }
-
-                if (numberTwoDayPass == 1) {
-                    productList.push(numberTwoDayPass + ' Pase por 2 Días');
-                }
-                else if (numberTwoDayPass > 1) {
-                    productList.push(numberTwoDayPass + ' Pases por 2 Días');
-                }
-
-                if (numberShirt == 1) {
-                    productList.push(numberShirt + ' Camisa');
-                }
-                else if (numberShirt > 1) {
-                    productList.push(numberShirt + ' Camisas');
-                }
-
-                if (numberLabels == 1) {
-                    productList.push(numberLabels + ' Paquete de Etiquetas');
-                }
-                else if (numberLabels > 1) {
-                    productList.push(numberLabels + ' Paquetes de Etiquetas');
-                }
-
-                productList.push('Más la ' + gift.value + ' de Regalo');
                 
+            calculate.addEventListener('click', calculateAmount);
 
-                result.innerHTML = '';
-                for (let i = 0; i < productList.length; i++) {
-                    result.innerHTML += productList[i];
-                    result.innerHTML += '</br>';
+            dayPass.addEventListener('blur', showCheckbox);
+            allDayPass.addEventListener('blur', showCheckbox);
+            twoDayPass.addEventListener('blur', showCheckbox);
+
+
+            nameUser.addEventListener('blur', emptyCamp);
+            surnameUser.addEventListener('blur', emptyCamp);
+            emailUser.addEventListener('blur', emptyCamp);
+            emailUser.addEventListener('blur', checkMail);
+
+
+
+            function calculateAmount(event) {
+                event.preventDefault();
+                if (gift.value === '') {
+                    alert('Debes elegir un regalo');
+                    gift.focus();
+                }
+                else {
+                    dayPass = document.getElementById('day-pass');
+                    allDayPass = document.getElementById('all-day-pass');
+                    twoDayPass = document.getElementById('two-days-pass');
+
+                    shirt = document.getElementById('camisa-evento');
+                    labels = document.getElementById('etiquetas');
+
+                    numberDayPass = dayPass.value;
+                    numberAllDayPass = allDayPass.value;
+                    numberTwoDayPass = twoDayPass.value;
+                    numberShirt = shirt.value;
+                    numberLabels = labels.value;
+
+                        
+
+                    totalAmountTickets = 0;
+
+                    totalAmountTickets = numberDayPass * 30 + numberAllDayPass * 50
+                        + numberTwoDayPass * 45 + numberShirt * 10 * 0.93
+                        + numberLabels * 2;
+                        
+                    productList = [];
+                    // Add products to Array
+                    if (numberDayPass == 1) {
+                        productList.push(numberDayPass + ' Pase por Día');
+                    }
+                    else if (numberDayPass > 1) {
+                        productList.push(numberDayPass + ' Pases por Día');
+                    }
+
+                    if (numberAllDayPass == 1) {
+                        productList.push(numberAllDayPass + ' Pase Completo');
+                    }
+                    else if (numberAllDayPass > 1) {
+                        productList.push(numberAllDayPass + ' Pases Completos');
+                    }
+
+                    if (numberTwoDayPass == 1) {
+                        productList.push(numberTwoDayPass + ' Pase por 2 Días');
+                    }
+                    else if (numberTwoDayPass > 1) {
+                        productList.push(numberTwoDayPass + ' Pases por 2 Días');
+                    }
+
+                    if (numberShirt == 1) {
+                        productList.push(numberShirt + ' Camisa');
+                    }
+                    else if (numberShirt > 1) {
+                        productList.push(numberShirt + ' Camisas');
+                    }
+
+                    if (numberLabels == 1) {
+                        productList.push(numberLabels + ' Paquete de Etiquetas');
+                    }
+                    else if (numberLabels > 1) {
+                        productList.push(numberLabels + ' Paquetes de Etiquetas');
+                    }
+
+                    productList.push('Más la ' + gift.value + ' de Regalo');
+                        
+
+                    result.innerHTML = '';
+                    for (let i = 0; i < productList.length; i++) {
+                        result.innerHTML += productList[i];
+                        result.innerHTML += '</br>';
+                    }
+
+                    total.innerHTML = totalAmountTickets;
+                }
+            }
+
+            function showCheckbox() {
+
+                showDays = [];
+                if (dayPass.value > 0) {
+                    showDays = ['viernes'];
+                }
+                else {
+                    document.getElementById('viernes').style.display = 'none';
                 }
 
-                total.innerHTML = totalAmountTickets;
-            }
-        }
+                if (twoDayPass.value > 0) {
+                    showDays = ['viernes', 'sabado'];
+                }
+                else {
+                    document.getElementById('sabado').style.display = 'none';
+                }
 
-        function showCheckbox() {
+                if (allDayPass.value > 0) {
+                    showDays = ['viernes', 'sabado', 'domingo'];
+                }
+                else {
+                    document.getElementById('domingo').style.display = 'none';
+                }
 
-            showDays = [];
-            if (dayPass.value > 0) {
-                showDays = ['viernes'];
-            }
-            else {
-                document.getElementById('viernes').style.display = 'none';
-            }
-
-            if (twoDayPass.value > 0) {
-                showDays = ['viernes', 'sabado'];
-            }
-            else {
-                document.getElementById('sabado').style.display = 'none';
+                for (let i = 0; i < showDays.length; i++) {
+                    document.getElementById(showDays[i]).style.display = 'block';
+                }
             }
 
-            if (allDayPass.value > 0) {
-                showDays = ['viernes', 'sabado', 'domingo'];
-            }
-            else {
-                document.getElementById('domingo').style.display = 'none';
-            }
+            function emptyCamp() {
+                if (this.value == '') {
+                    errorP.style.display = 'block';
+                    errorP.innerHTML = 'Este Campo es Obligatorio';
+                    this.style.border = '2px solid red'
+                    if (this == nameUser) camp1Complete = false;
+                    if (this == surnameUser) camp2Complete = false;
+                    if (this == emailUser) camp3Complete = false;
+                }
+                else {
+                    this.style.border = '1.75px solid #929292';
 
-            for (let i = 0; i < showDays.length; i++) {
-                document.getElementById(showDays[i]).style.display = 'block';
-            }
-        }
-
-        function emptyCamp() {
-            if (this.value == '') {
-                errorP.style.display = 'block';
-                errorP.innerHTML = 'Este Campo es Obligatorio';
-                this.style.border = '2px solid red'
-                if (this == nameUser) camp1Complete = false;
-                if (this == surnameUser) camp2Complete = false;
-                if (this == emailUser) camp3Complete = false;
-            }
-            else {
-                this.style.border = '1.75px solid #929292';
-
-                if (this == nameUser) camp1Complete = true;
-                if (this == surnameUser) camp2Complete = true;
-                if (this == emailUser) camp3Complete = true;
-                
-                if (camp1Complete && camp2Complete && camp3Complete) errorP.style.display = 'none';
+                    if (this == nameUser) camp1Complete = true;
+                    if (this == surnameUser) camp2Complete = true;
+                    if (this == emailUser) camp3Complete = true;
+                        
+                    if (camp1Complete && camp2Complete && camp3Complete) errorP.style.display = 'none';
+                };
             };
-        };
 
-        function checkMail() {
-            if (this.value.indexOf('@') < 0 && camp3Complete) {
-                errorPMail.style.display = 'block';
-                errorPMail.innerHTML = 'Ingrese un email Válido';
-                this.style.border = '2px solid red'
-            }
-            else {
-                errorPMail.style.display = 'none';
-                this.style.border = '1.75px solid #929292';
+            function checkMail() {
+                if (this.value.indexOf('@') < 0 && camp3Complete) {
+                    errorPMail.style.display = 'block';
+                    errorPMail.innerHTML = 'Ingrese un email Válido';
+                    this.style.border = '2px solid red'
+                }
+                else {
+                    errorPMail.style.display = 'none';
+                    this.style.border = '1.75px solid #929292';
+                }
             }
         }
-
     });
 })();
-*/
