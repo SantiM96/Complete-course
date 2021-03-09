df98fb726e5424be0f1c293383de4b39258d6a1a
SantiM96 <smt093@gmail.com>
2021-03-09 20:17:48 -0300

    change credentials

--- includes/functions/conection.php
+++ includes/functions/conection.php
@@ -1,6 +1,5 @@
 <?php 
-    $conn = new mysqli('localhost', 'root', 'root', 'gdlwebcamp');
+    $conn = new mysqli('sql10.freesqldatabase.com', 'sql10397857', 'tGRtErhefe', 'gdlwebcamp');
     if($conn->connect_error):
         echo $error->$conn->connect_error;
     endif; 
-    $hola = 3;
